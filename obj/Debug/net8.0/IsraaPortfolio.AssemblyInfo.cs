27f07f36be3668b02798873bb01ee1978ae35134
esraalwasity15 <esraalwasity15@gmail.com>
2026-03-28 02:05:37 +0300

    Added static build files

--- obj/Debug/net8.0/IsraaPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/IsraaPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IsraaPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b15878bfbb91128c6d2defe93a82651cd8a9be")]
 [assembly: System.Reflection.AssemblyProductAttribute("IsraaPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IsraaPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
